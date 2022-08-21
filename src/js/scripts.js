5ecd77b9b21d769ce37d0d64d5ac753f18862b22
Bruno Baroni <brunobaronims@gmail.com>
2022-08-21 13:11:14 -0300

    implemented on click functionality

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -27,7 +27,7 @@ function generateCardList() {
 
 function setCards(cardList, ul) {
   cardList.forEach((Card, index) => {
-    const cardFaces = {front: "../public/images/front.png", back: `../public/images/${cardList[index]}parrot.gif`};
+    const cardFaces = { front: "../public/images/front.png", back: `../public/images/${cardList[index]}parrot.gif` };
     const li = document.createElement('li');
     li.setAttribute('onclick', 'flipCard(event)');
     const inner = document.createElement('div');
@@ -35,15 +35,53 @@ function setCards(cardList, ul) {
     Object.entries(cardFaces).forEach((Item => {
       const div = document.createElement('div');
       div.classList.add(`flip-card-${Item[0]}`);
-      inner.appendChild(Object.assign(div, {innerHTML: `<img src=${Item[1]}></img>`}));
+      inner.appendChild(Object.assign(div, { innerHTML: `<img src=${Item[1]}></img>` }));
     }));
     li.appendChild(inner);
     ul.appendChild(li);
   })
 };
 
+function getClickedCard(event) {
+  if (event.target.onclick === null) {
+    let li = event.target.parentElement;
+    if (Array.from(li.parentElement.classList).length === 0) {
+      return li;
+    } else {
+      return li.parentElement;
+    }
+  } else {
+    return event.target;
+  }
+}
+
 function flipCard(event) {
-  console.log(event.target);
+  const card = getClickedCard(event);
+  if (card.className != 'flip-card-inner flipped' && card.className != 'flip-card-inner paired' && card.tagName != 'LI') {
+    console.log(card);
+    const uniqueCards = Array.from(document.getElementsByClassName('flipped'));
+    const pairedCards = Array.from(document.getElementsByClassName('paired'));
+    const flippedCards = uniqueCards.concat(pairedCards);
+    const flippedGifs = uniqueCards.slice();
+    flippedGifs.forEach((Card, index) => {
+      flippedGifs[index] = Card.innerHTML;
+    })
+    if (flippedCards.length % 2 === 0 && pairedCards.length >= flippedCards.length - 1) {
+      card.classList.add('flipped');
+    } else if (pairedCards.length === flippedCards.length - 1){
+      if (flippedGifs.includes(card.innerHTML)) {
+        uniqueCards.forEach((Card, index) => {
+          uniqueCards[index].classList.add('paired');
+          uniqueCards[index].classList.remove('flipped');
+        })
+        card.classList.add('paired');
+      } else {
+        card.classList.add('flipped');
+        setTimeout(() => {card.classList.remove('flipped')}, 1000);
+        setTimeout(() => {flippedCards.forEach(Card =>{Card.classList.remove('flipped')})}, 1000);
+      }
+    }
+  }
 }
 
 window.onload = (event) => {
