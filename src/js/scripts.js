40885abab70069ac6963bcd61165cc90137d037d
Bruno Baroni <brunobaronims@gmail.com>
2022-08-22 12:34:14 -0300

    cleaned syntax, added comments

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -63,11 +63,15 @@ function getClickedCard(event) {
 }
 
 function playAgain() {
-  const resposta = prompt('Você gostaria de jogar novamente?');
+  const resposta = prompt("Você gostaria de jogar novamente? Responda 'sim' ou 'não'.");
   if (resposta === 'sim') {
     const ul = document.querySelector('ul');
     const cardList = generateCardList();
+
+    //Reset counter
     document.querySelector('ul').dataset.count = "0";
+
+    // Clear card list
     ul.innerHTML = '';
     shuffle(cardList);
     setCards(cardList, ul);
@@ -80,34 +84,47 @@ function playAgain() {
 
 function flipCard(event) {
   const card = getClickedCard(event);
-  if (card.className != 'flip-card-inner flipped' && card.className != 'flip-card-inner paired' && card.tagName != 'LI') {
-    const uniqueCards = Array.from(document.getElementsByClassName('flipped'));
+  if (card.className != 'flip-card-inner peeked' && card.className != 'flip-card-inner paired' && card.tagName != 'LI') {
+    
+    // Flipped but not paired
+    const unpairedCards = Array.from(document.getElementsByClassName('peeked'));
+
+    // Flipped and paired
     const pairedCards = Array.from(document.getElementsByClassName('paired'));
-    const flippedCards = uniqueCards.concat(pairedCards);
-    const flippedGifs = uniqueCards.slice();
+
+    // All flipped cards
+    const flippedCards = unpairedCards.concat(pairedCards);
+    const flippedGifs = unpairedCards.slice();
     flippedGifs.forEach((Card, index) => {
       flippedGifs[index] = Card.innerHTML;
     })
+
+    // Clicked card starts a new pair
     if (flippedCards.length % 2 === 0 && pairedCards.length >= flippedCards.length - 1) {
-      card.classList.add('flipped');
+      card.classList.add('peeked');
       document.querySelector('ul').dataset.count++;
-    } else if (pairedCards.length === flippedCards.length - 1) {
+    } else if (pairedCards.length === flippedCards.length - 1) { // Check if player is trying to flip more than 2 cards at once
+      
+      // Flipped card forms a pair
       if (flippedGifs.includes(card.innerHTML)) {
         let count = document.querySelector('ul').dataset.count;
         count++;
-        uniqueCards.forEach((Card, index) => {
-          uniqueCards[index].classList.add('paired');
-          uniqueCards[index].classList.remove('flipped');
+        unpairedCards.forEach((Card, index) => {
+          unpairedCards[index].classList.add('paired');
+          unpairedCards[index].classList.remove('peeked');
         })
         card.classList.add('paired');
+
+        // All cards have been flipped
         if (Array.from(document.querySelectorAll('li')).length - 2 === pairedCards.length) {
           alert(`Você ganhou em ${count} jogadas!`);
           playAgain();
         }
-      } else {
-        card.classList.add('flipped');
-        setTimeout(() => { card.classList.remove('flipped') }, 1000);
-        setTimeout(() => { flippedCards.forEach(Card => { Card.classList.remove('flipped') }) }, 1000);
+      } else { // Flipped card does not form a pair
+
+        card.classList.add('peeked');
+        setTimeout(() => { card.classList.remove('peeked') }, 1000);
+        setTimeout(() => { flippedCards.forEach(Card => { Card.classList.remove('peeked') }) }, 1000);
         document.querySelector('ul').dataset.count++;
       }
     }
